8c948a5d29059842fc3c51b70da9ef1779e301e9
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-27 00:09:19 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newContainer = document.getElementById("create-task-form");
+  const container = ul();
+
+const add = document.querySelector("#new-task-description")
+   newContainer.addEventListener("submit", (e) => {
+    e.preventDefault();
+   
+    container.new(add.value)
+    e.target.reset;
+    function insert(){
+      const ulList = document.getElementById("tasks")
+      ulList.innerHTML = container.tasks() 
+    }
+  })
 });
+
+
+
