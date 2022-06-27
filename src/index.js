a28aa1e9a5178dcd57af63eb80f825f92932327c
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-27 09:35:41 +0300

    reset

--- src/index.js
+++ src/index.js
@@ -11,13 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const add = document.querySelector("#new-task-description").value
 
  
-   
+   //creating a list of inputs
       const ulList = document.createElement("li")
       ulList.innerText = add 
 
       tasks.append(ulList);
-
-      
+    //adding a reset functionality 
+      form.reset()
+//adding a delete functionality
       deletebtn.innerText = "x" 
       console.log(deletebtn)
       ulList.append(deletebtn)
@@ -25,9 +26,9 @@ document.addEventListener("DOMContentLoaded", () => {
       deletebtn.addEventListener("click", function(e){
    console.log(e.target)
    e.target.parentElement.remove()
- })
+ });
 
-      form.reset
+     
    })
  })
 
