cb6fbd0222a81b13ca63cf64eb8a2728e66ff1ab
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-27 09:30:53 +0300

    delete

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.getElementById("create-task-form");
   const tasks = document.getElementById("tasks");
+  const deletebtn = document.createElement("button")
 
 
    form.addEventListener("submit", (e) => {
@@ -15,9 +16,24 @@ document.addEventListener("DOMContentLoaded", () => {
       ulList.innerText = add 
 
       tasks.append(ulList);
+
+      
+      deletebtn.innerText = "x" 
+      console.log(deletebtn)
+      ulList.append(deletebtn)
+
+      deletebtn.addEventListener("click", function(e){
+   console.log(e.target)
+   e.target.parentElement.remove()
+ })
+
       form.reset
    })
  })
+
+ 
+
+
 
 
 
